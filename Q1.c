514e0da2dd0a5fa70d675546186d9d6247986bed
bhotoia <34321683+bhotoia@users.noreply.github.com>
2017-12-14 21:42:51 -0500

    Fixed error regarding deliverable

--- Q1.c
+++ Q1.c
@@ -1,21 +1,26 @@
 //Assignment 3 Question 1
 //OS Design and Systems Programming
 //December 15th 2017
-//Amrit Bhotoia and Yilitaer Yiliyaerxi
+//Amrit Bhotoia and Yilitaerxi Yiliyaer
 
-#include <stdio.h>#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <stdint.h>
  
 int* data;
-int amount, size, sum =0 , min, max, i;
- 
-void *sumNumbers(void *vargp)   //Thread for sum
+int amount, size, avg= 0, sum =0 , min, max, i;
+
+
+
+void *avgNumbers(void *vargp)   //Thread for avg
 {
     for(i = 0; i < size; i++)  
     {
-        sum = sum + data[i];        //Find sum of numbers for command line inputs.
+        sum = sum + data[i];        //Find avg of numbers for command line input
     }
+    
+    avg = sum/size;
 }
  
 void *maxNumbers(void *vargp)   //Thread for max
@@ -29,10 +34,9 @@ void *maxNumbers(void *vargp)   //Thread for max
     }
 }
  
-void *minNumbers(void *vargp)   //Thread for minimum
+void *minNumbers(void *vargp)   //Thread for min
 {  
     min = data[1];
- 
    
      for(i=0; i<size; i++)
     {
@@ -62,23 +66,23 @@ int main(int argc, char *argv[])
         sscanf( argv[i + 1], "%d", &data[i] );  //Convert string to an integer
     }
    
-    pthread_t sumNum;                                   //Initialize thread
-    pthread_create(&sumNum, NULL, sumNumbers, NULL);    //Create thread
-    pthread_join(sumNum, NULL);                         //Join thread
+    pthread_t avgNum;                                   //Initialize thread
+    pthread_create(&avgNum, NULL, avgNumbers, NULL);    //Create thread
+    pthread_join(avgNum, NULL);                         //Join thread
    
-    printf("Sum of Numbers: %d\n", sum);
+    printf("Average of Numbers: %d\n", avg);            //Outputs Average
    
     pthread_t maxNum;                                   //Initialize thread
     pthread_create(&maxNum, NULL, maxNumbers, NULL);    //Create thread
     pthread_join(maxNum, NULL);                         //Join thread
    
-    printf("Max of Numbers: %d\n", max);
+    printf("Max of Numbers: %d\n", max);                //Outputs Max
    
     pthread_t minNum;                                   //Initialize thread
     pthread_create(&minNum, NULL, minNumbers, NULL);    //Create thread
     pthread_join(minNum, NULL);                         //Join thread
    
-    printf("Min of Numbers: %d\n", min);
+    printf("Min of Numbers: %d\n", min);                //Outputs Min
    
    
     exit(0); //Exit Program
